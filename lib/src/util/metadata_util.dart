932eac0031a05006a352794ea9777c8ed7c70aae
copydog <775150@qq.com>
2019-03-01 12:27:07 +0800

    build: add mobx to package

--- lib/src/util/metadata_util.dart
+++ lib/src/util/metadata_util.dart
@@ -3,6 +3,7 @@ import 'package:source_gen/source_gen.dart';
 
 class MetadataUtil {
   static bool isType(ElementAnnotation annotation, Type type) {
-    return ConstantReader(annotation.computeConstantValue()).instanceOf(TypeChecker.fromRuntime(type));
+    return ConstantReader(annotation.computeConstantValue())
+        .instanceOf(TypeChecker.fromRuntime(type));
   }
 }
